4818153b261b18d700328ae7aada05d0f3f366e6
guolong.chen <guolong.chen@rokid.com>
2018-01-03 19:47:28 +0800

    add demo mk and fix demo install error

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -2,44 +2,46 @@ package com.example.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.rokid.voicerec.BearKidAdapter;
 import com.rokid.voicerec.BearKidCallback;
 import com.rokid.voicerec.CustomWord;
 
 public class MainActivity extends Activity implements BearKidCallback {
-	public static final String TAG = "MainActivity";
-	BearKidAdapter bearKidAdapter = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		bearKidAdapter = new BearKidAdapter();
-		bearKidAdapter.initialize(getApplicationContext(), "com.rokid.openvoice.VoiceService", this);
-		Log.e(TAG, bearKidAdapter.addCustomWord(CustomWord.TYPE_VT_WORD, "大傻逼", "da4sha3bi1") + "           --------------------");
-		Log.e(TAG, bearKidAdapter.queryCustomWord(-1).toString());
-	}
+    public static final String TAG = "BEAR_KID_ACTIVITY";
 
-	@Override
-	public void onException(int arg0) {
-		
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public void onIntermediateResult(String arg0) {
-		
-	}
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BearKidAdapter bearKidAdapter = new BearKidAdapter();
+                boolean initialize = bearKidAdapter.initialize(getApplicationContext(), "com.rokid.openvoice.VoiceService", MainActivity.this);
+                int i = bearKidAdapter.addCustomWord(CustomWord.TYPE_VT_WORD, "小祥祥", "xiao3xiang2xiang2");
+            }
+        }).start();
+    }
 
-	@Override
-	public void onRecognizeResult(String arg0, String arg1, String arg2) {
-		
-	}
+    @Override
+    public void onVoiceEvent(int i, int i1, double v) {
 
-	@Override
-	public void onVoiceEvent(int arg0, int arg1, double arg2) {
-		
-	}
+    }
+
+    @Override
+    public void onIntermediateResult(String s) {
+
+    }
+
+    @Override
+    public void onRecognizeResult(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    public void onException(int i) {
+
+    }
 }
